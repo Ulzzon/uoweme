744d2edd1123d774f2efa392ccf1820dd4f9ec8d
root <tobbe.olzzon@gmail.com>
2015-10-26 21:55:57 +0100

    Adding payments suggestions (Not done)

---
+++ app/src/main/java/com/example/tobbe/uoweme/PaymentActivity.java
@@ -0,0 +1,7 @@
+package com.example.tobbe.uoweme;
+
+/**
+ * Created by TobiasOlsson on 15-10-26.
+ */
+public class PaymentActivity {
+}
---
+++ app/src/main/java/com/example/tobbe/uoweme/adapters/PaymentsAdapter.java
@@ -0,0 +1,7 @@
+package com.example.tobbe.uoweme.adapters;
+
+/**
+ * Created by TobiasOlsson on 15-10-26.
+ */
+public class PaymentsAdapter {
+}
